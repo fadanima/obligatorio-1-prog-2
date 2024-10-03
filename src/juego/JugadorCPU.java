9d1773df0303c213bda287b22725a798b91ec1f9
Esteban Moroy <moroyesteban@gmail.com>
2024-10-02 22:29:40 -0300

    se implementa clase jugadorCPU extendiendo desde clase Jugador

---
+++ src/juego/JugadorCPU.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juego;
+
+/**
+ *
+ * @author Esteban Moroy 338885, Facundo Martinez
+ */
+
+public class JugadorCPU extends Jugador {
+
+    public JugadorCPU(String nombre, int edad, String alias) {
+        super(nombre, edad, alias);
+    }
+
+    /**
+     * Genera una jugada para la CPU y retorna un array de Strings con valores de
+     * coordenada y miniCoordenada
+     **/
+    public String[] generarJugada(Tablero tablero) {
+        // Simplemente se retorna una jugada aleatoria
+        String[] coordenadas = { "A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3" };
+        String[] miniCoordenadas = { "A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3" };
+
+        String coordenada = coordenadas[(int) (Math.random() * coordenadas.length)];
+        String miniCoordenada = miniCoordenadas[(int) (Math.random() * miniCoordenadas.length)];
+
+        return new String[] { coordenada, miniCoordenada };
+    }
+
+}
